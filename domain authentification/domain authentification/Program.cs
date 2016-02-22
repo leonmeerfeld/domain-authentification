f641da691f560dae3ccdf3c2882a097db273e17e
leonmeerfeld <leon.meerfeld@gmail.com>
2016-02-22 09:49:55 +0100

    Added my VS project files.

---
+++ domain authentification/domain authentification/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_authentification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Username:");
+            string username = Console.ReadLine();
+            Console.Write("Password:");
+            string password = "";
+
+            ConsoleKeyInfo key;
+
+            do
+            {
+                //Password masking:
+                key = Console.ReadKey(true);
+
+                if (key.Key != ConsoleKey.Enter && key.Key != ConsoleKey.Backspace)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
+                else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password = password.Substring(0, password.Length - 1);
+                    Console.Write("\b \b");
+                }
+            }
+            while (key.Key != ConsoleKey.Enter);
+
+            Console.Write("\n");
+
+            authentification a = new authentification();
+
+            if (a.validate_credentials(username, password))
+            {
+                Console.WriteLine("Credentials are correct");
+            }
+            else
+            {
+                Console.WriteLine("Credentials are wrong");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ domain authentification/domain authentification/authentification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices.AccountManagement;
+
+namespace Domain_authentification
+{
+    class authentification
+    {
+        public bool validate_credentials(string username, string password)
+        {
+            using (PrincipalContext context = new PrincipalContext(ContextType.Domain))
+            {
+                return context.ValidateCredentials(username, password);
+            }
+        }
+    }
+}
